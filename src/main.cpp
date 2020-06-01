0b6e1d2dc5a9dd46d006e62fc61453259a5ee37c
alexsu <supengcc@163.com>
2020-06-01 18:54:00 +0800

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,19 @@
 //
 // Created by alex on 2020/4/12.
 //
+#include <ros/ros.h>
 #include <depth_cluster.h>
 #include <iostream>
+
+DepthCluster depthCluster(2, 0.2, 16, 20);
+ros::Publisher point_pub;
+ros::Publisher ground_pub;
 
 void callback(const sensor_msgs::PointCloud2ConstPtr &cloud_ptr) {
     pcl::PointCloud<pcl::PointXYZI>::Ptr laserCloudIn;
     laserCloudIn.reset(new pcl::PointCloud<pcl::PointXYZI>());
     pcl::fromROSMsg(*cloud_ptr, *laserCloudIn);
 
-
-    sensor_msgs::PointCloud2 laserCloudTemp;
-    pcl::toROSMsg(*ground_points, laserCloudTemp);
-    laserCloudTemp.header.stamp = cloud_ptr->header.stamp;
-    laserCloudTemp.header.frame_id = cloud_ptr->header.frame_id;
-    ground_pub.publish(laserCloudTemp);
-
-    auto cluster_indices = depthCluster.getMergedClustersIndex();
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cluster_points(new pcl::PointCloud<pcl::PointXYZI>());
-    pcl::copyPointCloud(*laserCloudIn, cluster_indices, *cluster_points);
-    pcl::toROSMsg(*cluster_points, laserCloudTemp);
-    laserCloudTemp.header.stamp = cloud_ptr->header.stamp;
-    laserCloudTemp.header.frame_id = cloud_ptr->header.frame_id;
-    point_pub.publish(laserCloudTemp);
-
-};
-int main(int argc, char **argv) {
-
-
-    DepthCluster depthCluster(2, 0.2, 16, 20);
     depthCluster.setInputCloud(laserCloudIn);
     vector<vector<int>> clustersIndex = depthCluster.getClustersIndex();
     for (auto cluster_vec:clustersIndex) {
@@ -47,4 +32,27 @@ int main(int argc, char **argv) {
     pcl::PointCloud<pcl::PointXYZI>::Ptr ground_points(new pcl::PointCloud<pcl::PointXYZI>());
     pcl::copyPointCloud(*laserCloudIn, ground_index, *ground_points);
 
+    sensor_msgs::PointCloud2 laserCloudTemp;
+    pcl::toROSMsg(*ground_points, laserCloudTemp);
+    laserCloudTemp.header.stamp = cloud_ptr->header.stamp;
+    laserCloudTemp.header.frame_id = cloud_ptr->header.frame_id;
+    ground_pub.publish(laserCloudTemp);
+
+    auto cluster_indices = depthCluster.getMergedClustersIndex();
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cluster_points(new pcl::PointCloud<pcl::PointXYZI>());
+    pcl::copyPointCloud(*laserCloudIn, cluster_indices, *cluster_points);
+    pcl::toROSMsg(*cluster_points, laserCloudTemp);
+    laserCloudTemp.header.stamp = cloud_ptr->header.stamp;
+    laserCloudTemp.header.frame_id = cloud_ptr->header.frame_id;
+    point_pub.publish(laserCloudTemp);
+
+};
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "depth_cluster");
+    ros::NodeHandle nh;
+    ros::NodeHandle nh_local("~");
+    ros::Subscriber point_sub = nh.subscribe("velodyne_points", 1, callback);
+    point_pub = nh.advertise<sensor_msgs::PointCloud2>("cluster_result", 1);
+    ground_pub = nh.advertise<sensor_msgs::PointCloud2>("ground_result", 1);
+    ros::spin();
 }
